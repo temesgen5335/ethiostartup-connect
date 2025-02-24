24d493eaa9e0beb17021ea30f611ac630e845ffe
temesgen5335 <temesgen5335@gmail.com>
2025-02-24 14:52:04 +0300

    main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// This is the application's entry point
 createRoot(document.getElementById("root")!).render(<App />);
