292ea74475073a5c47e5e4babdc2518e8bf1c786
temesgen5335 <temesgen5335@gmail.com>
2025-02-24 15:17:53 +0300

    update the main hero page by removing the dupplication of routing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,34 +1,18 @@
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
 import { Footer } from "@/components/Footer";
-import Forums from "@/pages/Forums";
-import ThreadView from "@/components/ThreadView";
-import NewDiscussion from "@/components/NewDiscussion";
 
 const Index = () => {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <Features />
-              </>
-            } />
-            <Route path="/forums" element={<Forums />} />
-            <Route path="/forums/thread/:threadId" element={<ThreadView />} />
-            <Route path="/forums/new" element={<NewDiscussion />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
