77c7d0992a37c32ae6f900f71f0f00a3885e3449
temesgen5335 <temesgen5335@gmail.com>
2025-02-24 14:51:19 +0300

    updating all neccessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Forums from "./pages/Forums";
+import ThreadView from "./components/ThreadView";
+import NewDiscussion from "./components/NewDiscussion";
+import Network from "./pages/Network";
+import Resources from "./pages/Resources";
+import Jobs from "./pages/Jobs";
+import JobDetails from "./pages/JobDetails";
+import PostJob from "./pages/PostJob";
+import CompanyProfile from "./pages/CompanyProfile";
+import Events from "./pages/Events"
 
 const queryClient = new QueryClient();
 
@@ -16,7 +26,16 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/forums" element={<Forums />} />
+          <Route path="/forums/thread/:threadId" element={<ThreadView />} />
+          <Route path="/forums/new" element={<NewDiscussion />} />
+          <Route path="/network" element={<Network />}  />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
+          <Route path="/jobs/post" element={<PostJob />} />
+          <Route path="/companies/:id" element={<CompanyProfile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
