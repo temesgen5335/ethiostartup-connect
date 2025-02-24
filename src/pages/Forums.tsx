27d9e0f79914b464a04133dd5fa65eb0926ac4e8
temesgen5335 <temesgen5335@gmail.com>
2025-02-24 15:22:10 +0300

    first phase code review update

--- src/pages/Forums.tsx
+++ src/pages/Forums.tsx
@@ -1,4 +1,3 @@
-
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { ForumsContent } from "@/components/ForumsContent";
@@ -7,7 +6,8 @@ const Forums = () => {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <main className="pt-24 pb-20">
+      <main className="container mx-auto px-4 pt-24">
+        <h1 className="text-3xl font-bold mb-6">Forums</h1>
         <ForumsContent />
       </main>
       <Footer />
